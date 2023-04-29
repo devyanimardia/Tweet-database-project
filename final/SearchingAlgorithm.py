7c6bf6608280f001b2080d638889c9dff37df02f
zhxyn6 <zhxyn6@gmail.com>
2023-04-29 01:19:42 +0000

    Packaged class

--- final/SearchingAlgorithm.py
+++ final/SearchingAlgorithm.py
@@ -26,29 +26,30 @@ class SearchMong(object):
             ]
         )
 
-    def quote_tweet(self, quote_id):
+    def quote_tweet_id(self, quote_id):
         myquery_quote = {"tweet_id": {"$eq": None}}
         myquery_quote['tweet_id']['$eq'] = quote_id
         mydoc_quote = self.quote_tweet.find(myquery_quote)
-        sam = list(mydoc_quote)[0]
-        return [sam["tweet_id"], sam["text"], sam["username"], sam["favorite_count"]]
+        result = list(mydoc_quote)[0]
+        return result
 
-    def retweet_tweet(self, retweet_id):
+    def retweet_tweet_id(self, retweet_id):
         myquery_retweet = {"tweet_id": {"$eq": None}}
         myquery_retweet['tweet_id']['$eq'] = retweet_id
         mydoc_retweet = self.retweet_tweet.find(myquery_retweet)
-        sam = list(mydoc_retweet)[0]
-        return [sam["tweet_id"], sam["text"], sam["username"], sam["favorite_count"]]
+        result = list(mydoc_retweet)[0]
+        return result
 
-    def reply_tweet(self, reply_id):
+    def reply_tweet_id(self, reply_id):
         myquery_reply = {"tweet_id": {"$eq": None}}
         myquery_reply['tweet_id']['$eq'] = reply_id
         mydoc_reply = self.reply_tweet.find(myquery_reply)
         result = list(mydoc_reply)[0]
-        return [result["tweet_id"], result["text"], result["username"], result["favorite_count"]]
+        return result
 
     def recursive_reply(self, key, reply_rec_list):
-        reply_rec_list.append(self.reply_tweet(key))
+        m = self.reply_tweet_id(key)
+        reply_rec_list.append(m)
         myquery_rec_reply = {"tweet_id": {"$eq": None}}
         myquery_rec_reply['tweet_id']['$eq'] = key
         mydoc_rec_reply = list(self.reply_tweet.find(myquery_rec_reply))
@@ -59,71 +60,90 @@ class SearchMong(object):
                     self.recursive_reply(each_reply, reply_rec_list)
         return reply_rec_list
 
-    def keyword_search_in_reply(self, search_str, num_origin_tweets):
+    def keyword_search_in_reply(self, search_str, num_origin_tweets, sort_in = "dsc"):
         criteria = "keyword"
         sort_method = "timestamp_ms"
+        if sort_in == "desc":
+            sort_by = -1
+        else:
+            sort_by = 1
         if criteria == "keyword":
             myquery = {"$text": {"$search": None}}
             myquery['$text']['$search'] = search_str
-            mydoc_replies = self.reply_tweet.find(myquery).sort(sort_method, -1).limit(num_origin_tweets)
+            mydoc_replies = self.reply_tweet.find(myquery).sort(sort_method, sort_by).limit(num_origin_tweets)
         n = []
-        print("\n Search in reply yields: \n")
+
         for each_tweet in list(mydoc_replies):
             n.append(each_tweet)
         return n
 
     def top_10(self):
-        print("Random top tweets")
         most_famous = self.original_tweet.find().sort("custom_score", -1).limit(10)
         return list(most_famous)
 
-    def search_algorithm(self, search_string, criteria, sort_method="timestamp_ms", num_origin_tweets=10,
-                         num_replies=10, num_quotes=10, num_retweets=10, min_res_count=10):
+    def search_algorithm(self, search_string, criteria, sort_method='timestamp_ms', num_origin_tweets=10,
+                         num_replies=10, num_quotes=10, num_retweets=10, min_res_count=10, sort_in = dsc):
         ori_tweet = []
         quoted_rec_list = []
         retweeted_rec_list = []
         reply_rec_list = []
-
+        keyword_replies = []
+        if sort_in == "desc":
+            sort_by = -1
+        else:
+            sort_by = 1
         if criteria in ["keyword", "hashtags", "tweet_id"]:
             if criteria == "keyword":
                 myquery = {"$text": {"$search": None}}
                 myquery['$text']['$search'] = search_string
-                mydoc = self.original_tweet.find(myquery, {'score': {'$meta': 'textScore'}}).sort(
-                    [('score', {'$meta': 'textScore'})]).limit(num_origin_tweets)
+                if sort_method == "default":
+                    mydoc = self.original_tweet.find(myquery, {'score': {'$meta': 'textScore'}}).sort(
+                        [('score', {'$meta': 'textScore'})]).limit(num_origin_tweets)
+                else:
+                    mydoc = self.original_tweet.find(myquery).sort(sort_method, sort_by).limit(num_origin_tweets)
             if criteria == "hashtags":
                 myquery = {"hashtags.text": {"$eq": None}}
                 myquery['hashtags.text']['$eq'] = search_string
-                mydoc = self.original_tweet.find(myquery).sort(sort_method, -1).limit(num_origin_tweets)
+                mydoc = self.original_tweet.find(myquery).sort(sort_method, sort_by).limit(num_origin_tweets)
             if criteria == "tweet_id":
                 myquery = {"tweet_id": {"$eq": None}}
                 myquery['tweet_id']['$eq'] = search_string
-                mydoc = self.original_tweet.find(myquery).sort(sort_method, -1).limit(num_origin_tweets)
+                mydoc = self.original_tweet.find(myquery).sort(sort_method, sort_by).limit(num_origin_tweets)
         list_of_results_original = list(mydoc)
-        
+
         for each_tweet in list_of_results_original:
             ori_tweet.append(each_tweet)
             try:
                 for each_reply in range(min(len(each_tweet["reply_list"]), num_replies)):
-                    reply_rec_list = self.recursive_reply(each_tweet["reply_list"][each_reply], reply_rec_list=[])
-                    [print(i) for i in reply_rec_list]
+                    reply_rec_list = self.recursive_reply(each_tweet["reply_list"][each_reply], [])
             except:
-                print("No further replies needed")
+                pass
 
             try:
                 for each_quote in range(min(len(each_tweet["quoted_list"]), num_quotes)):
-                    quoted_rec_list.append(self.quote_tweet(each_tweet["quoted_list"][each_quote]))
+                    quoted_rec_list.append(self.quote_tweet_id(each_tweet["quoted_list"][each_quote]))
             except:
-                print("No further quoted tweets needed")
+                pass
 
             try:
                 for each_retweet in range(min(len(each_tweet["retweeted_list"]), num_retweets)):
-                    retweeted_rec_list.append(self.retweet_tweet(each_tweet["retweeted_list"][each_retweet]))
+                    retweeted_rec_list.append(self.retweet_tweet_id(each_tweet["retweeted_list"][each_retweet]))
             except:
-                print("No further retweets needed")
+                pass
 
         if len(list_of_results_original) < min_res_count:
             num = 10 - len(list_of_results_original)
-            self.keyword_search_in_reply(search_string, num_origin_tweets=num)
+            keyword_replies = self.keyword_search_in_reply(search_string, num_origin_tweets=num)
 
-        return ori_tweet, reply_rec_list, quoted_rec_list, retweeted_rec_list
+        return ori_tweet, reply_rec_list, quoted_rec_list, retweeted_rec_list, keyword_replies
 
+# n = SearchMong()
+# n.search_algorithm("quran", "keyword")
+# n.top_10()
+# n.search_algorithm("1253927939713966086","tweet_id", "timestamp_ms", 1)
+# n.search_algorithm("quran", "keyword", sort_method = "default")
+# n.search_algorithm("quran", "keyword", sort_method = "default", sort_in = "asc")
+
+# n.search_algorithm("coffee","hashtags")
+
+# n.search_algorithm("covid","keyword", num_origin_tweets = 1)
